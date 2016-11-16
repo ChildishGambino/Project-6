71b9d082c3e4e08d2600919ec9c2ff93330ea864
Clayton Lett <mr.fantastic1003@gmail.com>
2016-11-16 09:06:21 -0800

    Completed part 2

--- project6.js
+++ project6.js
@@ -1,12 +1,12 @@
-var BatX = 250;
+var playerX = 250;
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
-    BatX -=15;
- document.getElementById("bat").setAttribute("x", BatX)
+    playerX -=15;
+ document.getElementById("player").setAttribute("x", playerX)
 
   }
-  else if(e.keyCode == 39){
-        BatX +=15;
- document.getElementById("bat").setAttribute("x", BatX)
+  else if (e.keyCode == 39){
+        playerX +=15;
+ document.getElementById("player").setAttribute("x", playerX)
   }
 })
