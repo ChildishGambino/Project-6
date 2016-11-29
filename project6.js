377a0d96c7213df88dd85f38191950984ff52595
Clayton Lett <mr.fantastic1003@gmail.com>
2016-11-29 08:33:46 -0800

    part 4

--- project6.js
+++ project6.js
@@ -1,4 +1,5 @@
 var playerX = 250;
+var playerY = 250;
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
     playerX -=15;
@@ -8,5 +9,28 @@ document.addEventListener("keydown", function(e) {
   else if (e.keyCode == 39){
         playerX +=15;
  document.getElementById("player").setAttribute("x", playerX)
+
+
   }
+
+  else if (e.keyCode == 40){
+        playerY +=15;
+  document.getElementById("player").setAttribute("y", playerY)
+
+
+  }
+
+  else if (e.keyCode == 38){
+        playerY -=15;
+ document.getElementById("player").setAttribute("y", playerY)
+
+
+  }
+
+
 })
+
+var food1 = document.getElementById("food1")
+
+foodX = Number(food1.getAttribute("x"))
+foodY = Number(food1.getAttribute("y"))
